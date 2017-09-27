b7571bb12c3b3b2d4e3f3a154db3f4344190aeb2
Carlos Mario Arrieta Arenas <30849121+carrieta6@users.noreply.github.com>
2017-09-26 23:32:17 -0500

    1

---
+++ app/src/main/java/com/celular/android/tallercelular/Reporte2.java
@@ -0,0 +1,48 @@
+package com.celular.android.tallercelular;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Reporte2 extends AppCompatActivity {
+    private TableLayout tabla;
+    private ArrayList<Celular> celular;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporte2);
+
+        tabla=(TableLayout) findViewById(R.id.tabla);
+        celular = Datos.obtener();
+        for (int i = 0; i < celular.size() ; i++) {
+            TableRow fila = new TableRow(this);
+
+            if(celular.get(i).getMarca().equals("Samsung")&& ((celular.get(i).getCapacidadRam().equals("2GB"))||celular.get(i).getCapacidadRam().equals("3GB")||celular.get(i).getCapacidadRam().equals("4GB"))){
+                TextView c1 = new TextView(this);
+                TextView c2 = new TextView(this);
+                TextView c3 = new TextView(this);
+                TextView c4 = new TextView(this);
+                TextView c5 = new TextView(this);
+                TextView c6 = new TextView(this);
+                c2.setText(celular.get(i).getMarca());
+                c3.setText(celular.get(i).getCapacidadRam());
+                c4.setText(celular.get(i).getColor());
+                c5.setText(celular.get(i).getSO());
+                c6.setText(""+celular.get(i).getValor());
+
+                fila.addView(c1);
+                fila.addView(c2);
+                fila.addView(c3);
+                fila.addView(c4);
+                fila.addView(c5);
+                fila.addView(c6);
+                tabla.addView(fila);
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/celular/android/tallercelular/Reportes.java
@@ -0,0 +1,47 @@
+
+package com.celular.android.tallercelular;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Reportes extends AppCompatActivity {
+    private ListView lv;
+    private Resources resources;
+    private String opc[];
+    Intent in;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportes);
+        lv = (ListView) findViewById(R.id.opcionesreportes);
+        resources=this.getResources();
+        opc = resources.getStringArray(R.array.OpcionesReportes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,opc);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        in = new Intent(Reportes.this,Reporte1.class);
+                        startActivity(in);
+                        break;
+                    case 1:
+                        in = new Intent(Reportes.this,Reporte2.class);
+                        startActivity(in);
+                        break;
+                    case 2:
+                        in = new Intent(Reportes.this,Reporte3.class);
+                        startActivity(in);
+                        break;
+                }
+            }
+        });
+    }
+}
